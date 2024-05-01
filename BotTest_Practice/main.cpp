eb9855a22eb86fb61ca7118b82a246b280a6ec18
m5623skhj <m5623skhj@gmail.com>
2024-05-02 00:23:50 +0900

    * main문 수정

--- BotTest_Practice/main.cpp
+++ BotTest_Practice/main.cpp
@@ -1,16 +1,41 @@
 #include "PreCompile.h"
 #include <iostream>
+#include "BotActionManager.h"
 
-void InputButton();
-
-int main()
+bool Initialize()
 {
-	std::cout << "Run bot test" << std::endl;
+	if (BotActionManager::GetInst().Initialize() == false)
+	{
+		return false;
+	}
 
-	return 0;
+	return true;
 }
 
 void InputButton()
 {
 
+}
+
+int main()
+{
+	std::cout << "--------------------------------" << std::endl;
+	std::cout << "Initialize bot test" << std::endl;
+	std::cout << "--------------------------------" << std::endl << std::endl << std::endl;
+	if (Initialize() == false)
+	{
+		std::cout << "Initialize bot test failed" << std::endl;
+		return 0;
+	}
+
+	std::cout << "--------------------------------" << std::endl;
+	std::cout << "Start bot test" << std::endl;
+	std::cout << "--------------------------------" << std::endl << std::endl << std::endl;
+
+	
+	std::cout << "--------------------------------" << std::endl;
+	std::cout << "End bot test" << std::endl;
+	std::cout << "--------------------------------" << std::endl << std::endl << std::endl;
+
+	return 0;
 }
